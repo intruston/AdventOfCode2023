6ffd6b6e13fae9753e791854984ba03feace57d3
Vladimir Ivanov <intrustonly@gmail.com>
2023-12-18 17:24:47 +0100

    Updated sum and modified numbers

--- day1/task2.py
+++ day1/task2.py
@@ -5,19 +5,21 @@ def process_strings_from_file(file_path):
     modified_numbers = []
     total_sum = 0
 
+#words dictionary
     mapping = {n: str(i % 9 + 1) for i, n in enumerate('1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine'.split('|'))}
 
     with open(file_path, 'r') as file:
         for line in file:
+            #find all words and numbers (\d)
             matches = re.findall(r'(one|two|three|four|five|six|seven|eight|nine|\d)', line)
+            # Add the condition for "twone" explicitly
             all_extracted_numbers.extend(matches)
-
             modified_number = '00'
 
             if matches:
                 first_digit = next((match for match in matches if match.isdigit() or match.isalpha()), None)
 
-                spelled_out_numbers = [word for word in matches if word.isalpha()]
+                spelled_out_numbers = [word for word in matches if word.isalpha() or word.isdigit()]
 
                 if spelled_out_numbers and first_digit:
                     last_spelled_out = spelled_out_numbers[-1]
@@ -25,13 +27,14 @@ def process_strings_from_file(file_path):
                 elif first_digit:
                     modified_number = str(int(first_digit + first_digit)).zfill(2)
 
-            modified_numbers.append(modified_number)
+                total_sum += int(modified_number)  #accumulate the sum
 
-    total_sum = sum(int(num) for num in modified_numbers)
+            modified_numbers.append(modified_number)
 
     return all_extracted_numbers, modified_numbers, str(total_sum)
 
-file_path = 'input1.txt'
+#deal with twone and oneight manually
+file_path = 'input2.txt'
 extracted_numbers, modified_numbers, total_sum = process_strings_from_file(file_path)
 
 print("Extracted Numbers:", extracted_numbers)
